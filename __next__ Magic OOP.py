fc256fb22d12159948dce2a3cfc2c257f077714a
Biruk Lopiso Adore <birukabay1212@gmail.com>
2026-06-25 17:58:55 +0300

    Python OOP

---
+++ __next__ Magic OOP.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+my_array = np.array([1,2,3,4])
+print(my_array)
+
+#  Identify the type.
+print(type(my_array))
+print(my_array.shape)
+print(my_array.size)
+print(my_array+2)
+print(my_array-2)
+print(my_array*2)
