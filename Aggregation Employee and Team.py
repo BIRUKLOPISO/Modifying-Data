f62e6522192acdd5be9ba36009611c524fc73bce
Biruk Lopiso Adore <birukabay1212@gmail.com>
2026-06-30 10:37:45 +0300

    Aggregation Example in python OOP

---
+++ Aggregation Employee and Team.py
@@ -0,0 +1,21 @@
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class ShoppingCart:
+    def __init__(self, products):
+        self.products = products
+
+    def total_price(self):
+        return sum(p.price for p in self.products)
+
+
+p1 = Product("Exercise book", 100)
+p2 = Product("Bic Pen", 200)
+p3 = Product("New Bag", 300)
+
+products = [p1, p2, p3]
+
+shop = ShoppingCart(products)
+print(f"the total price is: ${shop.total_price()}")
